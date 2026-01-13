64205bad04aa8d78e21004aa65efffe83ab6a3fd
MarianaGallego <marianagallego97@hotmail.com>
2026-01-13 08:06:58 -0500

    475_done

---
+++ Seccion4/Seccion4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 10; i >= 0; i--, Console.WriteLine("Valor de salida: {0}", i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
